5c8b1a39014fd2d7a4dd850a732444cb72f2cf42
Yuki Itoh <yukiitohand@gmail.com>
2019-07-16 10:34:02 -0400

    main script is edited with component functions

--- artifact_correction_Carrozzo2016/main_artifact_correction.m
+++ artifact_correction_Carrozzo2016/main_artifact_correction.m
@@ -33,7 +33,7 @@ switch opt
 
 end
 
-
+load median_spectrum_vesta_Survey_I.mat sctime_info_vesta_mp idxes_valid isinc_lt_50_list img_spcs_meds
 
 %%
 % collect spectra
@@ -97,7 +97,8 @@ end
 %     toc;
 % end
 
-save vesta_Survey_median_spectrum.mat sctime_info_vesta_mp idxes_valid isinc_lt_50_list img_spcs_meds
+%%
+load median_spectrum_vesta_Survey_I.mat sctime_info_vesta_mp idxes_valid isinc_lt_50_list img_spcs_meds
 
 %% Remove odd and even effects
 % may be better to be performed after de-spiking procedure
@@ -106,30 +107,32 @@ im_med_t = squeeze(img_spcs_meds)';
 [im_med_t_smthd] = vir_oddeven_rmvl_wInterp(im_med_t,wv);
 
 %% Standard de-spiking procedure
-h = ones(3,1)/3;
-im_med_t_smthd_fil = imfilter(im_med_t_smthd,h);
+[im_med_t_smthd_outrmvd,outliers] = standard_despiking_Carrozzo2016(im_med_t_smthd,wv);
 
-r = im_med_t_smthd./im_med_t_smthd_fil;
-r_sgm = nanmean(((r-1).^2),'all');
-r_std = sqrt(r_sgm);
-
-outliers = abs(r-1) > 3*r_std;
-
-% interpolate outliers by a polynomial fit with 20 neighboring points.
-im_med_t_smthd_outrmvd = im_med_t_smthd;
-for c=1:256
-    for b=1:432
-        if outliers(b,c)
-            bdx_tested = max(1,(b-10)):min(432,(b+10));
-            x = wv(bdx_tested);
-            y = im_med_t_smthd(bdx_tested,c);
-            good_tmp = ~outliers(bdx_tested);
-            p = polyfit(x(good_tmp),y(good_tmp),2);
-            im_med_t_smthd_outrmvd(b,c) = polyval(p,wv(b));
-            
-        end
-    end
-end
+% h = ones(3,1)/3;
+% im_med_t_smthd_fil = imfilter(im_med_t_smthd,h);
+% 
+% r = im_med_t_smthd./im_med_t_smthd_fil;
+% r_sgm = nanmean(((r-1).^2),'all');
+% r_std = sqrt(r_sgm);
+% 
+% outliers = abs(r-1) > 3*r_std;
+% 
+% % interpolate outliers by a polynomial fit with 20 neighboring points.
+% im_med_t_smthd_outrmvd = im_med_t_smthd;
+% for c=1:256
+%     for b=1:432
+%         if outliers(b,c)
+%             bdx_tested = max(1,(b-10)):min(432,(b+10));
+%             x = wv(bdx_tested);
+%             y = im_med_t_smthd(bdx_tested,c);
+%             good_tmp = ~outliers(bdx_tested);
+%             p = polyfit(x(good_tmp),y(good_tmp),2);
+%             im_med_t_smthd_outrmvd(b,c) = polyval(p,wv(b));
+%             
+%         end
+%     end
+% end
 
 %% Unique median spectrum is obtained
 Umed = nanmedian(im_med_t_smthd_outrmvd,2);
@@ -140,12 +143,17 @@ Umed = nanmedian(im_med_t_smthd_outrmvd,2);
 [~,b1560] = bisection(wv-1.56);
 [~,b2410] = bisection(wv-2.41);
 [~,b2610] = bisection(wv-2.61);
+[~,b2590] = bisection(wv-2.59);
+[~,b3773] = bisection(wv-3.773);
 [~,b3740] = bisection(wv-3.74);
 [~,b3830] = bisection(wv-3.83);
 [~,b4350] = bisection(wv-4.35);
 [~,b4540] = bisection(wv-4.54);
 %%
-% 2.61-4.0um.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% vesta correction over 2.61-4.07um
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 2.61-4.0um for Ceres
 [~,b4200] = bisection(wv-4.2);
 [~,b2200] = bisection(wv-2.20);
 [~,b4070] = bisection(wv-4.07);
@@ -154,18 +162,45 @@ bdx_in = [b2200:b2410 b2610:b3740 b3830:b4200];
 % bdx_in = [b2610:b3740 b3830:b4070];
 x = wv(bdx_in);
 y = Umed(bdx_in);
-p_Umed_coeff = polyfit(x,y,5);
+% p_Umed_coeff = polyfit(x,y,5);
+p_Umed_coeff = polyfit(x,y,6);
 
 bdx_out = [b2610:b3740 b3830:b4070];
+bdx_out = [b2590:b3773 b3830:b4070];
 P_Umed = polyval(p_Umed_coeff,wv(bdx_out));
 
 figure; plot(wv,Umed); hold on; plot(wv(bdx_out),P_Umed);
 
-%% ARTIFACT MATRIX
+%% for print
+figure; plot(wv,Umed,'LineWidth',1,'Displayname','Median spectrum'); 
+hold on; 
+plot(wv(bdx_out),P_Umed,'LineWidth',1,'Displayname','Polynomial Fit');
+xlim([2.3 4.1]);
+% ylim([-0.02 0.3]);
+xlabel('Wavelength [{\mu}m]');
+ylabel('I/F');
+set(gca,'FontSize',14);
+legend
+export_fig('Figure_3.png','-transparent','-r150');
+
+
+%%
+%%ARTIFACT MATRIX
 % question: im_med_t_smthd_outrmvd or im_med_t_smthd ? 
 im_med_t_smthd_outrmvd_bdx_out = im_med_t_smthd_outrmvd(bdx_out,:);
 
-A = (im_med_t_smthd_outrmvd_bdx_out-P_Umed)./P_Umed;
+Aout = (im_med_t_smthd_outrmvd_bdx_out-P_Umed)./P_Umed;
+
+artifact_matrix = nan(I1Bdata.hdr.bands,I1Bdata.hdr.samples);
+artifact_matrix(bdx_out,:) = Aout;
+
+% save artifact_matrix_v2.mat artifact_matrix
+
+%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% ceres correction over 4.07-5.0um
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 
 %% TEST!!
 sctime = 366391692;
@@ -185,7 +220,7 @@ I1Bim_ifc = squeeze(I1Bim_if(:,c,:))';
 wv = I1Bdata.hdr.wavelength;
 [I1Bim_ifc_smthd] = vir_oddeven_rmvl_wInterp(I1Bim_ifc,wv);
 
-I1Bim_ifc_smthd_cor = I1Bim_ifc_smthd(bdx_out,:) ./ (1+A(:,c));
+I1Bim_ifc_smthd_cor = I1Bim_ifc_smthd(bdx_out,:) ./ (1+artifact_matrix(:,c));
 
 figure; plot(wv(bdx_out),I1Bim_ifc(bdx_out,:));
 figure; plot(wv(bdx_out),I1Bim_ifc_smthd(bdx_out,:));
